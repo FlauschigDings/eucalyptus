d74813f34f06058557ccd2f9aa407ad9626198d6
Flauschig <flauschig@flauschig.pet>
2024-05-20 01:13:01 +0200

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+    `maven-publish` // Apply the maven-publish plugin
 }
 
 group = "de.flauschig.eucalyptus"
@@ -11,6 +12,24 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://github.com/FlauschigDings/eucalyptus")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
+                password = project.findProperty("gpr.token") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
 
 tasks.test {
