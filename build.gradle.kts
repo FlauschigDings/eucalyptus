21af6c995bc531a2875327990af765b204d57371
Flauschig <flauschig@flauschig.pet>
2024-05-19 22:11:06 +0200

    add gradle publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+val pkgPath = "https://maven.pkg.github.com/FlauschigDings/eucalyptus"
+
 plugins {
     kotlin("jvm") version "1.9.23"
+    `maven-publish`
 }
 
 group = "de.flauschig.eucalyptus"
@@ -7,6 +10,31 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven {
+        url = uri(pkgPath)
+        credentials {
+            username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+            password = project.findProperty("gpr.token") as String? ?: System.getenv("GITHUB_TOKEN")
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri(pkgPath)
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.token") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("gpr") {
+            from(components["java"])
+        }
+    }
 }
 
 dependencies {
